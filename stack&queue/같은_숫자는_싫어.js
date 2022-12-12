c58aed3a7602ac73e80c967c3504ea7d87f7bc92
이서현 <seohyun.lee@lotte.net>
2022-12-12 21:53:24 +0900

    stack: 같은 숫자는 싫어

---
+++ stack&queue/같은_숫자는_싫어.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
+function solution(arr)
+{
+    var answer = [];
+    answer.push(arr[0]);
+    
+    for(let i = 1; i< arr.length ; i++) {
+        if(answer[answer.length - 1] !== arr[i]) answer.push(arr[i])
+    }
+    return answer;
+}
